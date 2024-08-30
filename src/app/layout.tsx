f7fc01a70fc71838ddf29b419641bb65ccada8b5
Ignacio Avilés <nacho72001@gmail.com>
2024-08-30 11:46:07 -0400

    feat: :sparkles: nuevas mejoras

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressContentEditableWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <Navbar />
