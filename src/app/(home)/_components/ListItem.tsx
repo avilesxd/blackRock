6967ad765827a4d54b194cc8efba3d2708e1c6fd
Ignacio Avilés <nacho72001@gmail.com>
2024-08-31 16:55:52 -0400

    style: :lipstick: pequeño cambio en el estilo

--- src/app/(home)/_components/ListItem.tsx
+++ src/app/(home)/_components/ListItem.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ListItem({ title, description }: { title: string, description: string }) {
   return (
     <li className="my-2">
-      <h2 className="font-semibold">{title}</h2>
+      <h2 className="font-semibold underline">{title}</h2>
       <p className="text-justify leading-relaxed">{description}</p>
     </li>
   )
