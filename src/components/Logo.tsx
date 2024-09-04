6a5f7bf3a1b54d41b2d370207a30ff9c3d42acb9
Ignacio Avilés <nacho72001@gmail.com>
2024-09-03 23:02:34 -0400

    fix: :lipstick: se corrigio un error con el logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function Logo() {
   return (
     <div>
-      <img className='h-12 w-15 dark:hidden' src="/logo.jpg" alt="Logo de Black Rock" />
-      <img className='h-12 w-15 hidden dark:block' src="/logoBlack.png" alt="Logo de Black Rock" />
-    </div>)
+      <img
+        className="h-12 w-15 dark:hidden"
+        src="/logoBlack.png"
+        alt="Logo de Black Rock"
+      />
+      <img
+        className="h-12 w-15 hidden dark:block"
+        src="/logo.png"
+        alt="Logo de Black Rock"
+      />
+    </div>
+  );
 }
 
-export default Logo
+export default Logo;
