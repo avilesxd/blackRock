bcd4c52da6576530620968b2db46e81ae76e5ecb
Ignacio Avilés <nacho72001@gmail.com>
2024-09-05 16:16:34 -0400

    refactor

--- src/app/about/_components/PersonCard.tsx
+++ src/app/about/_components/PersonCard.tsx
@@ -20,7 +20,7 @@ const PersonCard = ({ name, description, curriculum }: PersonCardProps) => {
     <div className="flex flex-col items-center dark:border-collapse border-2 border-black dark:border-teal-50 bg-white dark:bg-gray-800 dark:bg-gradient-to-b dark:from-slate-800 shadow-lg rounded-lg p-6 md:p-8 w-full space-y-4 dark:text-gray-100">
       <img
         className="rounded-full shadow-lg w-40 h-40 object-cover mb-4"
-        src={`/${name}.jpg`}
+        src={`/fotos/${name}.jpg`}
         alt={`Foto de ${name}`}
       />
       <TitleName name={name} />
